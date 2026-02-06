7648cab78180679fd285ac0238ca70d915232246
Wyatt Samuels <wsamuels13@icloud.com>
2026-02-06 17:17:11 -0500

    v1 movies api access

--- src/tmdb_client.py
+++ src/tmdb_client.py
@@ -8,14 +8,12 @@ load_dotenv()  # By default, looks for .env in the current directory
 # Get the API key
 api_key = os.getenv("TMDB_API_KEY")
 
-
-
-url = "https://api.themoviedb.org/3/authentication"
+url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
 
 headers = {
     "accept": "application/json",
-    "Authorization": "Bearer " + api_key
-}
+    "Authorization": "Bearer " + api_key 
+    }
 
 response = requests.get(url, headers=headers)
 
