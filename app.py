767dede202db33a141b7cee3c1d3e8489cbc7cbc
Wyatt Samuels <wsamuels13@icloud.com>
2026-02-27 16:07:51 -0500

    added route for movie vote

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from tmdb_client import fetch_movie_list
 
 app = Flask(__name__)
@@ -7,10 +7,12 @@ app = Flask(__name__)
 MOVIE_QUEUE = []
 LIKED_MOVIE = []
 
+# Home route that renders the main page of the app
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html') # CHANGE TO HOME PAGE ONCE SET UP
 
+# Sends the next movie in the queue to the frontend in JSON format
 @app.route('/api/get-next-movie')
 def get_next_movie():
 
@@ -29,13 +31,19 @@ def get_next_movie():
     
     return jsonify({"error": "No movies available"}), 404
 
-# @app.route('/api/like-movie/<int:movie_id>', methods=['POST'])
-# def like_movie(id):
+# Receive the user's input on whether they like or dislike the movie
+@app.route('/api/vote', methods=['POST'])
+def handle_vote():
+    data = request.json 
+    movie_id = data.get('id')
+    vote_type = data.get('vote')  # 'like' or 'dislike'
 
-    # list of movies liked by the user
-    # global LIKED_MOVIES
+    if vote_type == 'like':
+        # Add to liked movies list
+        LIKED_MOVIE.append(movie_id)
 
-    # LIKED_MOVIES.appen
+    return jsonify({"message": "Vote received", "liked_movies": LIKED_MOVIE})
+
 
     
 
