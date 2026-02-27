bb958c3c8ec51a83d840847e7755a611e02cd4fd
Brandon Romero <owenromerob@gmail.com>
2026-02-27 17:18:11 -0500

    fetch movie runtime and method fix

--- tmdb_client.py
+++ tmdb_client.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 api_key = os.getenv("TMDB_API_KEY")
-url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
 
 # fetches list of movies from api
 def fetch_movie_list():
+    url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
     headers = {
         "accept": "application/json",
         "Authorization": "Bearer " + api_key 
@@ -30,7 +30,7 @@ def fetch_movie_list():
     
 # fetches list of genres from api
 def fetch_genres():
-    genre_url = "https://api.themoviedb.org/3/genre/movie/list"
+    url = "https://api.themoviedb.org/3/genre/movie/list"
     headers = {
         "accept": "application/json",
         "Authorization": "Bearer " + api_key
@@ -45,3 +45,23 @@ def fetch_genres():
     except Exception as e:
         print(f"Error: {e}")
         return []
+    
+
+# fetch runtime of a movie by movie id
+def fetch_movie_runtime(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}"
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer " + api_key
+    }   
+
+    try: 
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+        return data.get('runtime', None)
+    
+    except Exception as e: 
+        print(f"Error fetching runtime: {e}")
+        return None
