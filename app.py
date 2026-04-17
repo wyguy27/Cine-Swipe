350bf491cb8a3907d7631fe56f724f567505c2b9
Ethan Hunt <ethandouglashunt@gmail.com>
2026-04-17 17:20:30 -0400

    Made the movies appear faster

--- app.py
+++ app.py
@@ -340,7 +340,7 @@ def get_next_movie():
         queue = movie_queues.get(user_key, [])
         page = movie_pages.get(user_key, 1)
 
-        if len(queue) < 5:
+        if len(queue) < 10:
             params = dict(filters)
             params["page"] = page
             batch = tmdb.fetch_movie_list(params)
@@ -380,7 +380,7 @@ def get_next_movie():
     queue = movie_queues.get(user_key, [])
     page = movie_pages.get(user_key, 1)
 
-    if len(queue) < 5:
+    if len(queue) < 10:
         filters["page"] = page
         batch = tmdb.fetch_movie_list(filters)
         queue.extend(batch)
